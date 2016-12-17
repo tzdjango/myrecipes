d4032a486cefa043caf4b37336ca6cb464fe7bf1
taza99 <tayyab.zaheer@yahoo.com>
2016-12-17 08:57:59 +0000

    Create new and edit forms and form partials

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
+  gem 'pry'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  
+  get '/home', to: 'pages#home'
+  
+  #get '/recipes', to: 'recipes#index' 
+  #get '/recipes/new', to: 'recipes#new', as: 'new_recipes'
+  #post '/recipes', to: 'recipes#create'
+  #get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
+  #patch '/recipes/:id', to: 'recipes#update'
+  #get '/recipes/:id', to: 'recipes#show', as: 'recipe'
+  #delete '/recipes/:id', to: 'recipes#destroy'
+  
+  resources :recipes
+  
   #get '/home', to: 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
