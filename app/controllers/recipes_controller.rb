04cbea3533207aa275c2a85b4cc60fba1932acf8
taza99 <tayyab.zaheer@yahoo.com>
2017-01-08 16:30:47 +0000

    Required login for like, modified controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,8 @@
 class RecipesController < ApplicationController
     
     before_action :set_recipe, only: [:edit, :update, :show, :like]
-    before_action :require_user, except: [:show, :index]
+    before_action :require_user, except: [:show, :index, :like]
+    before_action :require_user_like, only: [:like]
     before_action :require_same_user, only: [:edit, :update]
     
     
@@ -73,4 +74,11 @@ class RecipesController < ApplicationController
                 redirect_to recipes_path
             end
         end
+        
+        def require_user_like
+            if !logged_in?
+            flash[:danger] = "You must be logged in to perform that action"
+            redirect_to :back
+            end
+        end        
 end
